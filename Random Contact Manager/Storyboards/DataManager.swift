e4701188aa4ea779340d1ae1369a420282bf8ffa
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2021-02-17 19:52:10 +0300

    Build project. Create DataManager

---
+++ Random Contact Manager/Storyboards/DataManager.swift
@@ -0,0 +1,19 @@
+//
+//  DataManager.swift
+//  Random Contact Manager
+//
+//  Created by Виталий Сабин on 17.02.2021.
+//
+
+let names = ["Ned", "Robert", "John", "Joffrey", "Stannis",
+             "Oberrin", "Tirion", "Sansa", "Brandon", "Margery"]
+
+let surnames = ["Stark", "Martel", "Lannister", "Greyjoy", "Frey",
+                "Tirell", "Karstark", "Targaryen", "Arryn", "Tully"]
+
+let emails = ["first@mail.sk", "second@mail.sk", "third@mail.sk",
+              "some@email.sk", "new@mail.sk", "war@mail.sk",
+              "peace@mail.sk", "batman@mail.sk", "swift@mail.sk", "last@mail.sk"]
+
+let phonesNumbers = ["1234567890", "2345678901", "3456789012", "4567890123", "5678901234",
+                     "6789012345", "789012345", "8901234567", "9012345678", "0123456789"]
